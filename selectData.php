3f44b64c2c7a4994304583ee04df878922431779
rogermylife <rogermylifeQ@gmail.com>
2017-05-02 17:11:22 +0800

    select lots of data

--- selectData.php
+++ selectData.php
@@ -2,8 +2,31 @@
 
 require_once ('connect.php');
 
+echo "select one data\n";
+echo "--------------------\n";
 $sql="SELECT * FROM ctfquestions WHERE id=?";
 $stm = $pdo->prepare($sql);
 $stm->execute(array(rand()%10000));
 $result = $stm->fetch(PDO::FETCH_ASSOC);
 var_dump($result);
+
+echo "select all data\n";
+echo "--------------------\n";
+$sql="SELECT * FROM ctfquestions WHERE id < 5";
+$stm = $pdo->prepare($sql);
+$stm->execute();
+$result = $stm->fetchAll(PDO::FETCH_ASSOC);
+var_dump($result);
+
+echo "itrater data\n";
+echo "--------------------\n";
+$sql="SELECT * FROM ctfquestions WHERE id > 9995";
+$stm = $pdo->prepare($sql);
+$stm->execute();
+
+foreach($stm->fetchAll() as $row)
+    var_dump($row);
+
+$stm->execute();
+while($row = $stm->fetch())
+    var_dump($row);

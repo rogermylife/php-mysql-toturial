909de1c89f45d5396280dc1eac8a773dc4d14062
rogermylife <rogermylifeQ@gmail.com>
2017-05-02 16:16:54 +0800

    connect

---
+++ connect.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'config/settings.php';
+try {
+    $pdo = new PDO(
+        sprintf(
+            'mysql:host=%s;dbname=%s;port=%s;charset=%s',
+            $settings['host'],
+            $settings['name'],
+            $settings['port'],
+            $settings['charset']
+        ),
+        $settings['username'],
+        $settings['password']
+    );
+} catch (PDOException $e) {
+    echo "Database connection failed";
+    exit;
+}
+echo "Database connection success\n";
+
